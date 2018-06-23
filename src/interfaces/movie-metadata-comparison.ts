e71f70e45cfd1827b8669a1ba545c1eaaf3a66ba
Bronley Plumb <bronley@gmail.com>
2018-06-23 00:01:31 -0400

    Aligned with new movie description fields.

--- src/interfaces/movie-metadata-comparison.ts
+++ src/interfaces/movie-metadata-comparison.ts
@@ -1,36 +1,36 @@
 export interface MovieMetadataComparison {
-    incoming: MovieMetadata;
-    current: MovieMetadata;
+  incoming: MovieMetadata;
+  current: MovieMetadata;
 }
 export interface MovieMetadata {
-    backdropUrls: any[];
-    collection?: any;
-    collectionOrder?: any;
-    completionSeconds: number;
-    description: string;
-    extraSearchText: string[];
-    genres: string[];
-    keywords: string[];
-    posterUrls: any[];
-    rating: string;
-    releaseYear: number;
-    runtimeSeconds: number;
-    sortTitle: string;
-    summary: string;
-    title: string;
-    tmdbId: number;
-    cast: Cast[];
-    crew: Crew[];
+  backdropUrls: any[];
+  collection?: any;
+  collectionOrder?: any;
+  completionSeconds: number;
+  extraSearchText: string[];
+  genres: string[];
+  keywords: string[];
+  posterUrls: any[];
+  rating: string;
+  releaseYear: number;
+  runtimeSeconds: number;
+  sortTitle: string;
+  shortSummary: string;
+  summary: string;
+  title: string;
+  tmdbId: number;
+  cast: Cast[];
+  crew: Crew[];
 }
 
 export interface Cast {
-    tmdbId: number;
-    character: string;
-    name: string;
+  tmdbId: number;
+  character: string;
+  name: string;
 }
 
 export interface Crew {
-    tmdbId: number;
-    job: string;
-    name: string;
+  tmdbId: number;
+  job: string;
+  name: string;
 }
--- src/interfaces/movie.ts
+++ src/interfaces/movie.ts
@@ -1,23 +1,23 @@
 export interface Movie {
-    id: number;
-    mediaType: 'MOVIE';
-    title: string;
-    summary: string;
-    sourceId: number;
-    description: string;
-    runtimeSeconds: number;
-    resumeSeconds: number;
-    rating?: string;
-    releaseYear?: number;
-    tmdbId?: number;
-    posterUrls: string[];
-    backdropUrls: string[];
-    videoUrl: string;
+  id: number;
+  mediaType: 'MOVIE';
+  title: string;
+  sourceId: number;
+  shortSummary: string;
+  summary: string;
+  runtimeSeconds: number;
+  resumeSeconds: number;
+  rating?: string;
+  releaseYear?: number;
+  tmdbId?: number;
+  posterUrls: string[];
+  backdropUrls: string[];
+  videoUrl: string;
 }
 
 export interface CardMovie {
-    id: number;
-    posterUrls: string;
+  id: number;
+  posterUrls: string;
 }
 
 
