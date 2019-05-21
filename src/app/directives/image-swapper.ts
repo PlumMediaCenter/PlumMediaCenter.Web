e9b54efeb8549bb35608e9ae8d5b6545b64bac8f
Bronley Plumb <bronley@gmail.com>
2019-05-21 12:55:25 -0400

    Fixed many issues (bulk commit due to git corruption).

--- src/app/directives/image-swapper.ts
+++ src/app/directives/image-swapper.ts
@@ -1,4 +1,5 @@
 import { Directive, Input, ElementRef, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
+import { sleep } from '../util';
 
 
 @Directive({ selector: '[imageSwapper]' })
@@ -10,42 +11,47 @@ export class ImageSwapperDirective implements OnDestroy, OnInit {
     ) {
         this.img = this.el.nativeElement;
     }
+
+    private imageSwapInterval = 7000;
+    private fadeOutMilliseconds = 1010;
     private intervalHandle;
     private img: HTMLImageElement;
 
-    /**
-     * Determine if the parent view is currently visible
-     */
-    private get parentViewIsVisible() {
-        let parentComponent = (this.viewContainerRef as any)._view.component;
-        throw new Error('not implemented');
-        return false;
-        // TODO let activeComponent = this.navCtrl.getActive().instance;
-        // if (parentComponent === activeComponent) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-    }
-
     private currentIndex = -1;
-    private nextImage() {
+    private async nextImage() {
         this.currentIndex++;
         if (this.currentIndex >= this.urls.length) {
             this.currentIndex = 0;
         }
         let url = this.urls[this.currentIndex];
         if (url) {
+            //create a temporary absolutely positioned image element and fade it out
+            const faderImage = document.createElement('img');
+            //point to the "old" image and fade it out
+            faderImage.src = this.img.src;
+            Object.assign(faderImage.style, {
+                width: this.img.offsetWidth,
+                height: this.img.offsetHeight
+            });
+            faderImage.classList.add('fader-image');
+            this.img.parentElement.appendChild(faderImage);
+            await sleep(0);
             this.img.src = url;
+            await sleep(0);
+            faderImage.classList.add('transparent');
+            await sleep(this.fadeOutMilliseconds);
+            this.img.parentElement.removeChild(faderImage);
         }
     }
 
     ngOnInit(): void {
-        this.intervalHandle = setInterval(() => {
-            if (this.parentViewIsVisible) {
-                this.nextImage();
+        this.intervalHandle = setInterval(async () => {
+            //if this component is currently visible, swap to next image
+            if (this.el.nativeElement.offsetParent) {
+                console.log('next image');
+                await this.nextImage();
             }
-        }, 5000);
+        }, this.imageSwapInterval);
         //run the first nextImage call so we get an image right away
         this.nextImage();
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7586'
+  apiUrl: 'http://192.168.1.20:7586'
 };
